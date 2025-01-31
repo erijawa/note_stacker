029c0b9d571ddb2f2736e7bf81659cac3d1e6435
erijawa <e.akgw0913@gmail.com>
2025-01-31 11:18:15 +0900

    [Add] #5 プロバイダ（Google）の環境変数設定

--- front/auth.config.ts
+++ front/auth.config.ts
@@ -2,5 +2,8 @@ import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const authConfig: NextAuthConfig = {
-  providers: [ Google ]
+  providers: [ Google({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  }) ]
 }
