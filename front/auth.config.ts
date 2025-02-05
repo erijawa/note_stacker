4e11d2cbd493a99a30b9eb95566c7dd7e39aba76
erijawa <e.akgw0913@gmail.com>
2025-02-05 13:11:16 +0900

    [Refactor] 外部プロバイダの環境変数が自動読み込み可能と判明したため、記述を削除

--- front/auth.config.ts
+++ front/auth.config.ts
@@ -2,8 +2,5 @@ import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const authConfig: NextAuthConfig = {
-  providers: [ Google({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }) ]
+  providers: [ Google ]
 }
