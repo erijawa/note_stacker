1d603a5b08727cf09fcdcecb33722e504f871be3
erijawa <e.akgw0913@gmail.com>
2025-02-06 11:26:16 +0900

    [Add] #6 usersコントローラーの作成、ルーティング設定
    users#createのみ定義済みです

---
+++ back/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.find_by(provider: params[:provider], provider_id: params[:provider_id])
+    if !user
+      user = User.create(provider: params[:provider], provider_id: params[:provider_id], name: params[:name])
+    end
+    if user
+      head :ok
+    else
+      render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
+    end
+    rescue StandardError => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+end
