cf75f263be7fc1fac5b069f298013e424dd38342
erijawa <e.akgw0913@gmail.com>
2025-02-06 15:50:09 +0900

    [Fix] #6 APIの返却値を修正

--- back/app/controllers/api/v1/users_controller.rb
+++ back/app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.find_by(provider: params[:provider], provider_id: params[:provider_id])
-    if !user
-      user = User.create(provider: params[:provider], provider_id: params[:provider_id], name: params[:name])
+    @user = User.find_by(provider: params[:provider], provider_id: params[:provider_id])
+    if !@user
+      @user = User.create(provider: params[:provider], provider_id: params[:provider_id], name: params[:name])
     end
-    if user
-      head :ok
+    if @user
+      render json: @user, head :ok
     else
       render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
     end
