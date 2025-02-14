8b8e417228a6e10cc64ac030fc968bc75fcfa514
erijawa <e.akgw0913@gmail.com>
2025-02-14 14:51:39 +0900

    [Fix] #35 デザイン修正
    投稿カードで記事へのリンク部分がわかりやすいようhover時のbackground-colorを設定

--- front/components/ui/PostCard.tsx
+++ front/components/ui/PostCard.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export default function PostCard({ post }: Props) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 my-4 flex flex-col">
-      <Link href={post.url} target="_blank">
+    <div className="rounded-lg shadow-md p-4 my-4 flex flex-col">
+      <Link href={post.url} target="_blank" className="p-4 rounded-lg hover:bg-stone-100">
         <div className="flex items-start space-x-4">
           <img
             alt="Article thumbnail"
@@ -26,13 +26,13 @@ export default function PostCard({ post }: Props) {
           />
           <div className="space-y-2">
             {/* urlから取得した記事タイトルを反映予定 */}
-            <h2 className="text-xl font-bold text-[#16404D]">タイトル</h2>
+            <h2 className="text-xl font-bold ">タイトル</h2>
             <p className="text-gray-700">{/* {post.article.description} */}</p>
           </div>
         </div>
         {post.comment && (
-          <div className="mt-4 p-2 bg-gray-100 rounded-md">
-            <h3 className="text-lg font-semibold text-[#16404D]">コメント</h3>
+          <div className="mt-4 p-2 rounded-md">
+            <h3 className="text-lg font-semibold ">コメント</h3>
             <p className="text-gray-700">{post.comment}</p>
           </div>
         )}
